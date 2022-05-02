eaa94c31b639cc295bb427ff036ab372697908cc
guruputran <gpsmartcodes@gmail.com>
2022-05-02 23:46:30 +0800

    localizing more

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -29,6 +29,56 @@ const Home = () => {
           </div>
         </div>
       </form>
+      {/* <!-- Button trigger modal --> */}
+      <button
+        type="button"
+        className="btn bg-gradient-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      {/* <!-- Modal --> */}
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">Body modal</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn bg-gradient-danger"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn bg-gradient-warning">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
